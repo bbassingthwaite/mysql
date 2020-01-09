64319027fb3f906c8a30933bd07e00ed43276049
Braden Bassingthwaite <bbassingthwaite@digitalocean.com>
2020-01-09 13:55:45 -0600

    Update driver.go

--- driver.go
+++ driver.go
@@ -81,7 +81,7 @@ func (d MySQLDriver) Open(dsn string) (driver.Conn, error) {
 }
 
 func init() {
-	sql.Register("mysql", &MySQLDriver{})
+	sql.Register("mysql15", &MySQLDriver{})
 }
 
 // NewConnector returns new driver.Connector.
